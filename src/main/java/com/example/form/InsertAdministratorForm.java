0738fc961a7ec93d23c6786822aaf03aa05f20f9
yuri <yuriko.mori@rakus-partners.co.jp>
2024-08-21 15:06:08 +0900

    入力値チェック修正

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,6 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -15,9 +16,10 @@ public class InsertAdministratorForm {
 	private String name;
 	/** メールアドレス */
 	@Size(min = 1, max = 127, message = "Eメールは1文字以上127文字以内で記載してください")
+	@Email(message = "E メールの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
-	@Size(min = 1, max = 127, message = "パスワードは1文字以上127文字以内で記載してください")
+	@Size(min = 8, max = 16, message = "パスワードは8文字以上16文字以内で記載してください")
 	private String password;
 
 	public String getName() {
