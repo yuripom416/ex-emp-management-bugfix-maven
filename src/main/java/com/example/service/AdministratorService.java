6cec8f3c0b488d0045072f11d596be6d8199727e
yuri <yuriko.mori@rakus-partners.co.jp>
2024-08-22 14:08:18 +0900

    service_メールアドレスから管理者情報取得メソッド追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -40,4 +40,15 @@ public class AdministratorService {
 		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
 		return administrator;
 	}
+
+	/**
+	 * メールアドレスから管理者情報を取得する
+	 * 
+	 * @param mailAddress メールアドレス
+	 * @return 取得した管理者情報
+	 */
+	public Administrator findByMailAddress(String mailAddress) {
+		Administrator administrator = administratorRepository.findByMailAddress(mailAddress);
+		return administrator;
+	}
 }
